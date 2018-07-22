9f91dfc56ca52ec8cd58d94e257eac027546154f
R Srinivasan <RajaSrinivasan@hotmail.com>
2018-07-22 06:28:24 -0400

    changed GITHUB_TOKEN

--- setuprelease.sh
+++ setuprelease.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #export GITHUB_TOKEN="2cb0df7c3eb7223a2c202eff2aadf049adc634a4"
-export GITHUB_TOKEN="1034eadec0304a96452239318fce02fdde5184c3"
+export GITHUB_TOKEN=`1034eadec0304a96452239318fce02fdde5184c3`
