06bfbebf3d2dd2777bcd29357bfecc17f22c76a7
R Srinivasan <RajaSrinivasan@hotmail.com>
2018-07-24 06:26:05 -0400

    use proper unit test framework support

--- logfiles/dates_test.go
+++ logfiles/dates_test.go
@@ -13,17 +13,17 @@ func TestDateId(t *testing.T) {
 	} else {
 		t.Error("basic test failure")
 	}
-	fmt.Printf("Today Id %s should be %s\n", DateId(time.Now()), TodayId())
-	fmt.Printf("Yesterday %s\n", YesterdayId())
+	t.Logf("Today Id %s should be %s\n", DateId(time.Now()), TodayId())
+	t.Logf("Yesterday %s\n", YesterdayId())
 }
 
 func TestParseId(t *testing.T) {
 	str := "19860401"
 	t1, err := ParseId(str)
 	if err != nil {
-		fmt.Printf("Error %s parsing %s\n", err.Error(), str)
+		t.Logf("Error %s parsing %s\n", err.Error(), str)
 	}
-	fmt.Printf("%s results in %s\n", str, DateId(t1))
+	t.Logf("%s results in %s\n", str, DateId(t1))
 	if t1 == time.Date(1986, time.April, 1, 0, 0, 0, 0, time.UTC) {
 		t.Log("basic tests passed")
 	} else {
@@ -34,15 +34,15 @@ func TestParseId(t *testing.T) {
 func TestDateOfLogfile(t *testing.T) {
 	ts, err := DateOfLogfile("20180621_0208.log")
 	if err == nil {
-		fmt.Println(ts)
+		t.Log(ts)
 	} else {
-		fmt.Println(err)
+		t.Log(err)
 	}
 	ts, err = DateOfLogfile("20180621.log")
 	if err == nil {
-		fmt.Println(ts)
+		t.Log(ts)
 	} else {
-		fmt.Println(err)
+		t.Log(err)
 	}
 }
 
